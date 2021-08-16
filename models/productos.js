065918e4ebd4bb26216da17b51f6b3927a2a879f
Nico <87850895+NicoSalina@users.noreply.github.com>
2021-08-16 02:32:22 -0300

    PROYECTO v.2

--- models/productos.js
+++ models/productos.js
@@ -6,10 +6,10 @@ const getAll = async() => {   //una ejecucion de una query tarde, es una promesa
     const rows = await pool.query(query, params);  //query es nuestra consulta y le paso los params (parametros) son los ?? de mi query
     return rows;
 }
-const getSingle = async(id) => {  // el single al ser una sola fila que la identifico por mi id o lo que quiera, le tengo que agergar un WHERE
-    const query = "SELECT p.id, p.nombre, p.descripcion, p.precio, p.stock, c.nombre AS nombreCategoria FROM ?? AS p JOIN ?? AS c ON p.id_categoria = c.id WHERE p.id = ? AND p.eliminado = 0";
+const getSingle = async(id) => { // el single al ser una sola fila que la identifico por mi id o lo que quiera, le tengo que agergar un WHERE
+    const query = "SELECT p.id, p.nombre, p.descripcion, p.precio, p.stock, p.id_categoria, c.nombre AS nombreCategoria FROM ?? AS p JOIN ?? AS c ON p.id_categoria = c.id WHERE p.id = ? AND p.eliminado = 0";
     const params = [process.env.T_PRODUCTOS, process.env.T_CATEGORIAS, id];
-    const rows = await pool.query(query, params);  //query es nuestra consulta y le paso los params (parametros) son los ?? de mi query
+    const rows = await pool.query(query, params); //query es nuestra consulta y le paso los params (parametros) son los ?? de mi query
     return rows;
 }
 const create = async(obj) => {
@@ -22,10 +22,16 @@ const update = async(id, obj) => {
     const params = [process.env.T_PRODUCTOS, obj, id];
     return await pool.query(query, params);
 }
-const del = async(id) => {    // ELIMINADO LOGICO
+const del = async(id) => { // ELIMINADO LOGICO
     const query = "UPDATE ?? SET eliminado = 1 WHERE id = ?";
     const params = [process.env.T_PRODUCTOS, id];
     return await pool.query(query, params);
 }
+const getNombre = async(nombre) => {
+    const query = "SELECT p.id, p.nombre, p.descripcion, p.precio, p.stock, p.id_categoria, c.nombre AS nombreCategoria FROM ?? AS p JOIN ?? AS c ON p.id_categoria = c.id WHERE p.nombre LIKE ? AND p.eliminado = 0";
+    const params = [process.env.T_PRODUCTOS, process.env.T_CATEGORIAS, nombre];
+    const rows = await pool.query(query, params);
+    return rows;
+}
 
-module.exports = {getAll, getSingle, create, update, del}
+module.exports = {getAll, getSingle, create, update, del, getNombre}
